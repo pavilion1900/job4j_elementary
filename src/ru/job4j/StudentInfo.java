1f344ad163df9f4c578016e400da2a6fdc6c2b97
pavilion1900 <pavilion1900@gmail.com>
2020-09-26 19:39:49 +0300

    3.3. Разбор первой программы. [#373737]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Korolenko");
+        System.out.println("20.04.1986");
+    }
+}
