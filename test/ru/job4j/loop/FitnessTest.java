8662fa557bcb86c3db3f6c2dbc34a7f62654c6c0
pavilion1900 <pavilion1900@gmail.com>
2020-10-03 23:56:01 +0300

    5.4. Протеиновая диета [#373774]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        int result = Fitness.cacl(95, 90);
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        int result = Fitness.cacl(90, 95);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        int result = Fitness.cacl(50, 90);
+        assertThat(result, is(2));
+    }
+}
