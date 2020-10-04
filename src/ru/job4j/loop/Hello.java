9630431aae2e0bddaacbc3210ec2757813ca84a3
pavilion1900 <pavilion1900@gmail.com>
2020-10-04 10:07:42 +0300

    Hello

--- src/ru/job4j/loop/Hello.java
+++ src/ru/job4j/loop/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
     public static void paint(int width, int height) {
         for (int row = 0; row <height; row++) {
             for (int cell=0; cell < width; cell++) {
-                if ((row+cell) % 2 == 0) {
+                if
+                ((row+cell) % 2 == 0) {
                     System.out.print("X");
                 } else
                     {
