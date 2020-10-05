6cd232deff7368a4efddb17ae2fda758ed308725
pavilion1900 <pavilion1900@gmail.com>
2020-10-05 21:40:40 +0300

    6.0.2. Заполнение массива. [#373699]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,17 @@ public class ArrayDefinition {
         System.out.println("Размер массива = " + ages.length);
         System.out.println("Размер массива = " + surnames.length);
         System.out.println("Размер массива = " + prices.length);
+        System.out.println();
+
+        String[] names = new String[4];
+        names[0] = "Sergey Korolenko";
+        names[1] = "Max Chernye";
+        names[2] = "Nikolay Frolov";
+        names[3] = "Pavel Mahnach";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
