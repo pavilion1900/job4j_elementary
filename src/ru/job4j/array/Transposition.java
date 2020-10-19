acd4834735d71ef6713b4fc3d1a63c77271b716e
pavilion1900 <pavilion1900@gmail.com>
2020-10-19 07:43:21 +0300

    17. Транспонировать матрицу

---
+++ src/ru/job4j/array/Transposition.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class Transposition {
+    public static int[][] convert(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] result = new int[m][n];
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                int temp = result[i][j];
+                result[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        return result;
+    }
+}
+
