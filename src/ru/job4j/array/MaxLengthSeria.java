8770bae94061cccd634b6cd7f603a881d101d464
pavilion1900 <pavilion1900@gmail.com>
2020-10-19 19:53:10 +0300

    22. Максимальная длина неубывающей последовательности

---
+++ src/ru/job4j/array/MaxLengthSeria.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class MaxLengthSeria {
+    public static int find(int[] array) {
+        int count = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] <= array[i]) {
+                count += 1;
+            } else {
+                count = 1;
+            }
+        }
+        return count;
+    }
+}
